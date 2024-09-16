71c43f83ba054e5cac76bd1d7178159fe5df8fb0
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-09-16 14:14:04 +0100

    Add layout and the page content

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,16 @@
-import Image from 'next/image';
-import image1 from '@/public/about-1.jpg';
-import image2 from '@/public/about-2.jpg';
+import Image from "next/image";
+import image1 from "@/public/about-1.jpg";
+import image2 from "@/public/about-2.jpg";
 
 export default function About() {
   return (
-    <div className='grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center'>
-      <div className='col-span-3'>
-        <h1 className='text-4xl mb-10 text-accent-400 font-medium'>
+    <div className="grid grid-cols-5 gap-x-24 gap-y-32 text-lg items-center">
+      <div className="col-span-3">
+        <h1 className="text-4xl mb-10 text-accent-400 font-medium">
           Welcome to The Wild Oasis
         </h1>
 
-        <div className='space-y-8'>
+        <div className="space-y-8">
           <p>
             Where nature&apos;s beauty and comfortable living blend seamlessly.
             Hidden away in the heart of the Italian Dolomites, this is your
@@ -32,30 +32,30 @@ export default function About() {
         </div>
       </div>
 
-      <div className='col-span-2'>
+      <div className="col-span-2">
         <Image
           src={image1}
-          alt='Family sitting around a fire pit in front of cabin'
-          placeholder='blur'
+          alt="Family sitting around a fire pit in front of cabin"
+          placeholder="blur"
           quality={80}
         />
       </div>
 
-      <div className='relative aspect-square col-span-2'>
+      <div className="relative aspect-square col-span-2">
         <Image
-          src='/about-2.jpg'
+          src="/about-2.jpg"
           fill
-          className='object-cover'
-          alt='Family that manages The Wild Oasis'
+          className="object-cover"
+          alt="Family that manages The Wild Oasis"
         />
       </div>
 
-      <div className='col-span-3'>
-        <h1 className='text-4xl mb-10 text-accent-400 font-medium'>
+      <div className="col-span-3">
+        <h1 className="text-4xl mb-10 text-accent-400 font-medium">
           Managed by our family since 1962
         </h1>
 
-        <div className='space-y-8'>
+        <div className="space-y-8">
           <p>
             Since 1962, The Wild Oasis has been a cherished family-run retreat.
             Started by our grandparents, this haven has been nurtured with love
@@ -73,8 +73,8 @@ export default function About() {
 
           <div>
             <a
-              href='/cabins'
-              className='inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all'
+              href="/cabins"
+              className="inline-block mt-4 bg-accent-500 px-8 py-5 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all"
             >
               Explore our luxury cabins
             </a>
