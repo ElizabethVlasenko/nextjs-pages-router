f0c9802d86e15569f87ffbdc06adc9a616dabe9e
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-09-16 14:20:28 +0100

    Add page title and favicon

--- pages/cabins/[cabinId].js
+++ pages/cabins/[cabinId].js
@@ -1,8 +1,18 @@
+import Head from "next/head";
 import { useRouter } from "next/router";
 
 function Cabin() {
   const router = useRouter();
-  return <div>Cabin #{router.query.cabinId}</div>;
+
+  return (
+    <>
+      <Head>
+        <title>Cabin #{router.query.cabinId} | The Wild Oasis</title>
+        <link rel="icon" href="logo.png" />
+      </Head>
+      <div>Cabin #{router.query.cabinId}</div>
+    </>
+  );
 }
 
 export default Cabin;
