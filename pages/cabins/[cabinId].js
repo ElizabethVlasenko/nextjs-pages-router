7227303ce905d007918701ed7d08e044f082f1f2
ElizabethVlasenko <elizabeth.wolker@gmail.com>
2024-09-16 15:21:54 +0100

    Fetch a cabin data using getServerSideProps (SSR)

--- pages/cabins/[cabinId].js
+++ pages/cabins/[cabinId].js
@@ -1,16 +1,32 @@
+import CabinView from "@/components/CabinView";
+import { getCabin } from "@/lib/data-service";
 import Head from "next/head";
-import { useRouter } from "next/router";
 
-function Cabin() {
-  const router = useRouter();
+//Dynamically generated (SSR)
+export async function getServerSideProps({ params }) {
+  const cabin = await getCabin(params.cabinId);
+  return {
+    props: {
+      cabin,
+    },
+    revalidate: 3600, //Incremental Static Regeneration (ISR)
+  };
+}
+
+// getStaticPaths + getStaticProps - generates static route that has dynamic segments
+
+function Cabin({ cabin }) {
+  // const router = useRouter();
 
   return (
     <>
       <Head>
-        <title>Cabin #{router.query.cabinId} | The Wild Oasis</title>
+        <title>Cabin #{cabin.name} | The Wild Oasis</title>
         <link rel="icon" href="logo.png" />
       </Head>
-      <div>Cabin #{router.query.cabinId}</div>
+      <div className="max-w-6xl mx-auto mt-8">
+        <CabinView cabin={cabin} />
+      </div>
     </>
   );
 }
